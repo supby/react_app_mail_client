68286d3910a7d7ba46bd160e26bb44ecace364f2
Suzanovich, Andre <andre.suzanovich@doverfs.com>
2018-07-18 16:29:35 +0200

    add page structure

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">Brand</a>
+            </div>
+            <div className="collapse navbar-collapse">
+              <form className="navbar-form navbar-left">
+                <div className="form-group">
+                  <input type="text" className="form-control" placeholder="Search" />
+                </div>
+              </form>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+        </div>
       </div>
     );
   }
