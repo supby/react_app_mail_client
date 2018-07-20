66f948a94c4d7fffb71810c4dc9e45ade2903cf9
Suzanovich, Andre <andre.suzanovich@doverfs.com>
2018-07-20 11:04:35 +0200

    add debounced search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 class SearchBar extends Component {
+    constructor() {
+        super();
+        this.state = { searchQuery: '' };
+
+        this.handleSearchDebounced = _.debounce(function () {
+            //this.props.handleSearch.apply(this, [this.state.query]);            
+        }, 500);
+    }
+
+    searchQueryChanged(event) {        
+        this.setState({searchQuery: event.target.value});
+        this.handleSearchDebounced();
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="form-group">
-                <input type="text" className="form-control" placeholder="Search" />
+                <input type="text"
+                    onChange={this.searchQueryChanged.bind(this)}
+                    value={this.state.searchQuery}
+                    className="form-control"
+                    placeholder="Search" />
             </div>
         );
     }
