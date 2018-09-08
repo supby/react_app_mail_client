5f487d2e9ad2a5ef2f19438aa065883150a3c5a3
Andre Suzanovich <supius@gmail.com>
2018-09-08 12:11:24 +0200

    pretify

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers'
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-theme.css'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
 const store = createStore(
     rootReducer,
@@ -16,8 +16,8 @@ const store = createStore(
 )
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById('root'));
-registerServiceWorker();
+  <Provider store={store}>
+    <App />
+  </Provider>,
+    document.getElementById('root'))
+registerServiceWorker()
