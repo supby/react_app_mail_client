6a0cb4efc0e896fe7cd23d223b5e77d37f0a10a7
Andre Suzanovich <supius@gmail.com>
2018-11-25 19:14:10 +0100

    handle login

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,75 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap';
 import './App.css'
-
 import SearchBar from './components/SearchBar'
 import EmailsList from './components/EmailsList'
 
 class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
   render () {
+    const { isAuthenticated } = this.props.auth;
+
     return (
       <div className='App'>
         <nav className='navbar navbar-default navbar-fixed-top'>
           <div className='container-fluid'>
             <div className='navbar-header'>
-              <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>
-                <span className='sr-only'>Toggle navigation</span>
-                <span className='icon-bar' />
-                <span className='icon-bar' />
-                <span className='icon-bar' />
-              </button>
               <a className='navbar-brand' href='#'>Brand</a>
             </div>
-            <div className='collapse navbar-collapse'>
-              <form className='navbar-form navbar-left'>
-                <SearchBar />
-              </form>
-            </div>
+            {
+              isAuthenticated() && (
+                <div className='collapse navbar-collapse'>
+                  <form className='navbar-form navbar-left'>
+                    <SearchBar />
+                    <Button
+                      id="qsLogoutBtn"
+                      bsStyle="primary"
+                      className="btn-margin"
+                      onClick={this.logout.bind(this)}>
+                      Log Out
+                    </Button>
+                  </form>
+                </div>
+              )
+            }
+
+            {
+              !isAuthenticated() && (
+                  <Button
+                    id="qsLoginBtn"
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.login.bind(this)}>
+                    Log In
+                  </Button>
+                )
+            }
           </div>
         </nav>
-        <div style={{marginTop: 100 + 'px'}} className='container'>
-          <div className='row'>
-            <div className='col-lg-2'>Left Pane</div>
-            <div className='col-lg-6'>
-              <EmailsList />
-            </div>
-            <div className='col-lg-2'>Right Pane</div>
-          </div>
-        </div>
+        {
+              isAuthenticated() && (
+                <div style={{marginTop: 100 + 'px'}} className='container'>
+                  <div className='row'>
+                    <div className='col-lg-2'>Left Pane</div>
+                    <div className='col-lg-6'>
+                      <EmailsList />
+                    </div>
+                    <div className='col-lg-2'>Right Pane</div>
+                  </div>
+                </div>
+              )
+          }
       </div>
     )
   }
--- client/src/index.js
+++ client/src/index.js
@@ -9,15 +9,34 @@ import 'bootstrap/dist/css/bootstrap-theme.css'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter, Route } from "react-router-dom";
+import Callback from './Callback/Callback';
+import Auth from './Auth/Auth';
 
 const store = createStore(
     rootReducer,
     applyMiddleware(thunk)
 )
 
+const auth = new Auth();
+
+const handleAuthentication = ({location}) => {
+  if (/access_token|id_token|error/.test(location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" render={(props) => <App auth={auth} {...props} />} />
+        <Route path="/callback" render={(props) => {
+              handleAuthentication(props);
+              return <Callback {...props} /> 
+        }}/>
+      </div>
+    </BrowserRouter>
   </Provider>,
     document.getElementById('root'))
 registerServiceWorker()
