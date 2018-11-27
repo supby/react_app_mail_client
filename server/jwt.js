9000f1d951617737b5ca5dcc4cba964c07d2ca35
Andre Suzanovich <supius@gmail.com>
2018-11-27 22:37:34 +0100

    beautify + gmail service

--- server/jwt.js
+++ server/jwt.js
@@ -1,18 +1,17 @@
-const jwt = require('express-jwt');
+const jwt = require('express-jwt')
 
-const jwksRsa = require('jwks-rsa');
-
+const jwksRsa = require('jwks-rsa')
 
 const checkJwt = jwt({
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: "https://andresuz.eu.auth0.com/.well-known/jwks.json"
-    }),
-    audience: 'http://mailapp',
-    issuer: "https://andresuz.eu.auth0.com/",
-    algorithms: ['RS256']
-});
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://andresuz.eu.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'http://mailapp',
+  issuer: 'https://andresuz.eu.auth0.com/',
+  algorithms: ['RS256']
+})
 
-module.exports = checkJwt;
+module.exports = checkJwt
